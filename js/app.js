02bd1f6c61a952c9c590806c085515e60d55825a
Igor Todorovic <igor333m@yahoo.com>
2018-07-12 22:08:48 -0700

    feat: Links on hamburger menu

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
+let hamburgerHome = document.getElementById('hamburger-home');
+let hamburgerAbout = document.getElementById('hamburger-about');
+let hamburgerProjects = document.getElementById('hamburger-projects');
+let hamburgerContact = document.getElementById('hamburger-contact');
 let home = document.getElementById('homeButton');
 let what = document.getElementById('whatButton');
 let projects = document.getElementById('projectsButton');
 let contact = document.getElementById('contactButton');
+let checkbox = document.getElementById('navi-toggle').checked;
+
 
 /**** Animate menu buttons on refresh ****/
 window.onload = function() {
@@ -38,7 +44,60 @@ projects.addEventListener('click', (e) => {
 
 contact.addEventListener('click', (e) => {
 	e.preventDefault();
-	document.querySelector('#contact').scrollIntoView({ 
+	document.querySelector('#contacts').scrollIntoView({ 
 	  behavior: 'smooth'
+	});
+});
+
+// Set navigaton checkbox to false
+function checkboxToglle() {
+		document.getElementById('navi-toggle').checked = false;
+}
+
+// Hamburger navigation
+hamburgerHome.addEventListener('click', (e) => {
+	e.preventDefault();
+	
+	checkboxToglle(checkbox);
+
+	console.log(checkbox);
+	document.querySelector('#header').scrollIntoView({ 
+	  behavior: 'smooth'
+	});
+});
+
+hamburgerAbout.addEventListener('click', (e) => {
+	e.preventDefault();
+	
+	checkboxToglle(checkbox);
+
+	console.log(checkbox);
+	document.querySelector('#what-am-i').scrollIntoView({ 
+	  behavior: 'smooth',
+	  block: 'start'
+	});
+});
+
+hamburgerProjects.addEventListener('click', (e) => {
+	e.preventDefault();
+	
+	checkboxToglle(checkbox);
+
+	console.log(checkbox);
+	document.querySelector('#projects').scrollIntoView({ 
+	  behavior: 'smooth',
+	  block: 'start'
+	});
+});
+
+hamburgerContact.addEventListener('click', (e) => {
+	e.preventDefault();
+	
+	checkboxToglle(checkbox);
+
+	console.log(checkbox);
+	document.querySelector('#contacts').scrollIntoView({ 
+	  behavior: 'smooth',
+	  block: 'start'
 	});
 });
