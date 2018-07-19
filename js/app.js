f319e6bac04e94264de5d1efba951acf9878f07e
Igor Todorovic <igor333m@yahoo.com>
2018-07-19 08:44:11 -0700

    refactor: Js cleanup

--- js/app.js
+++ js/app.js
@@ -98,12 +98,8 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         goToTop.classList.add('button-show');
-        console.info("yes");
     } else {
-  
-        goToTop.classList.remove('button-show');
-        console.info("no");
-
+      goToTop.classList.remove('button-show');
     }
 }
 
